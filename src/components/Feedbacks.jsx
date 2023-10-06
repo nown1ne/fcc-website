7d44f7491b8a5a64619d4a38b043bf46e2754ab8
Abhinav Srinivas <abhinav14863@gmail.com>
2023-10-06 23:03:18 +0530

    Update Feedbacks.jsx

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -15,7 +15,7 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className='bg-black-200 p-10 m-5 md:m-10 rounded-3xl xs:w-[320px] w-full'
+    className='bg-black-200 p-10 m-5 rounded-3xl xs:w-[320px] w-full'
   >
     <p className='text-white font-black text-[48px]'>"</p>
 
@@ -28,7 +28,7 @@ const FeedbackCard = ({
             <span className='blue-text-gradient'>@</span> {name}
           </p>
           <p className='mt-1 text-secondary text-[12px]'>
-            {designation} of {company}
+            {company}
           </p>
         </div>
 
