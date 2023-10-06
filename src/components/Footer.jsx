7da4b35b05a61f8bfe834a9c3a2f1fae37e51f57
Anurag Deo <110685873+Anurag-deo14@users.noreply.github.com>
2023-10-06 23:56:28 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,76 +1,38 @@
-import React from "react";
-import facebookIcon from "../assets/social.png";
-import twitterIcon from "../assets/twitter.png";
-import linkedinIcon from "../assets/linkedin.png";
-import instagramIcon from "../assets/instagram.png";
-import discordIcon from "../assets/discord.png";
+import React from 'react'
+import './Footer.css'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineGithub} from 'react-icons/ai'
+import {BsDiscord} from 'react-icons/bs'
+import { logo } from "../assets";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="text-center md:text-left mb-4 md:mb-0 md:mr-4">
-          <h2 className="text-2xl font-bold">FCC NITRR</h2>
-          <p className="text-sm">GE Road, Amanaka, Raipur</p>
-          <p className="text-sm">Email: financeclub@nitrr.ac.in</p>
-        </div>
-
-        <div className="text-center md:self-center md:mr-2">
-        <h3 className="text-xl font-semibold mb-2 md:mr-12 md:mb-12">Follow Us</h3>
-        <div className="flex items-center justify-center md:mr-12">
-          <a
-            href="https://github.com/FCC-NITRR"
-            className="text-2xl hover:text-[#915EFF] mx-2"
-          >
-            <img src={facebookIcon} alt="Facebook" style={{width:'35px'}} />
-          </a>
-        
-          <a
-            href="https://www.linkedin.com/company/finance-nitrr/"
-            className="text-2xl hover:text-[#915EFF] mx-2"
-          >
-            <img src={linkedinIcon} alt="LinkedIn" style={{width:'35px'}} />
-          </a>
-          <a
-            href="https://www.instagram.com/fccnitrr/"
-            className="text-2xl hover:text-[#915EFF] mx-2"
-          >
-            <img src={instagramIcon} alt="Instagram" style={{width:'35px'}} />
-          </a>
-          <a
-            href="https://discord.com/invite/B3pk6UdQ88"
-            className="text-2xl hover:text-[#915EFF] mx-2"
-          >
-            <img src={discordIcon} alt="Instagram" style={{width:'35px'}} />
-          </a>
-        </div>
+    <footer >
+      <a href='#' className='footer__logo'><img src={logo} alt='logo' className='w-29 h-12 object-contain' /></a>
+      
+      
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#sponsors'>Sponsors</a></li>
+        <li><a href='#events'>Events</a></li>
+        <li><a href='#projects'>Projects</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='footer__socials'>
+        <a href='https://www.linkedin.com/company/finance-nitrr/'><AiFillLinkedin  size={35}/></a>
+        <a href='https://www.instagram.com/fccnitrr/'><AiOutlineInstagram size={35}/></a>
+        <a href='https://github.com/FCC-NITRR'><AiOutlineGithub size={35}/></a>
+        <a href='https://discord.com/invite/B3pk6UdQ88'><BsDiscord size={35}/></a>
       </div>
 
-        <div className="text-center md:ml-4 md:self-center md:mt-6">
-          <h3 className="text-xl font-semibold mb-2">Quick Links</h3>
-          <ul className="text-sm">
-            <li className="mb-1">
-              <a href="#" className="hover:text-[#915EFF]">Home</a>
-            </li>
-            <li className="mb-1">
-              <a href="#about" className="hover:text-[#915EFF]">About</a>
-            </li>
-            <li className="mb-1">
-              <a href="#events" className="hover:text-[#915EFF]">Events</a>
-            </li>
-            <li className="mb-1">
-              <a href="#contact" className="hover:text-[#915EFF]">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="mt-6 text-center md:ml-4">
-        <p className="text-sm">&copy; {new Date().getFullYear()} FCC. All rights reserved.</p>
-        <p className="text-sm">Made with 💜 by the Tech Team </p>
+      
+      <div className='footer__copyright'>
+        <medium>Made with 💜 by Tech Team.</medium>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
